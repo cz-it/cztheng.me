094ad8a0af63f7bac59b4700e6a2d0ae11536763
CZ <cz.theng@gmail.com>
2020-11-12 16:00:30 +0800

    [TASK] about_cz to about

--- cztheng.me/deploy.sh
+++ cztheng.me/deploy.sh
@@ -3,6 +3,8 @@
 rm -rf ../www/*
 hugo -d ../www -v
 #mkdir ../www/images
+cp -rf ../www/about_cz/index.html ../www/about
 cp -rf ../logo.png ../www/images
 cp -rf ../favicon.png ../www
 cp -rf ../favicon.png ../www/favicon.ico
+
